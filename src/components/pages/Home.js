5c420c5e39ef3c2231d9e9ebfa4efa1a4b6d074f
Maria Ortega <mariaortegapb@gmail.com>
2021-03-23 23:52:03 -0400

    added routes

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my portfolio</h1>
+      <p>
+        My name is Maria Ortega. Currently, I am learning at the coding boot
+        camp at UNC Charlotte. My e-mail is mariaortegapb@gmail.com and my phone
+        number is (336)244-2641.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
